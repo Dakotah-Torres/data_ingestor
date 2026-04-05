0d659423a016dbfbe277af793185e4e5b8fd519f
Dakotah-Torres <dtorres@dtdevtech.com>
2026-04-05 18:47:22 -0500

    added price and quantity parser

---
+++ src/parser/mod.rs
@@ -0,0 +1,13 @@
+pub fn extract_price(raw: &str) -> Option<&str> {
+    let key = "\"p\":\"";
+    let start = raw.find(key)? + key.len();
+    let end = raw[start..].find('\"')? + start;
+    Some(&raw[start..end])
+}
+
+pub fn extract_quantity(raw: &str) -> Option<&str> {
+    let key = "\"q\":\"";
+    let start = raw.find(key)? + key.len();
+    let end = raw[start..].find('\"')? + start;
+    Some(&raw[start..end])
+}
