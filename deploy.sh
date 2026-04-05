6950444334b08375ced2d57a8aee63807760b881
Dakotah-Torres <dtorres@dtdevtech.com>
2026-04-05 18:57:35 -0500

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e 
+
+MESSAGE="$1"
+
+if [ -z "$MESSAGE"]; then
+    echo "Usage : ./deploy.sh \" your commit message\""
+    exit 1 
+fi 
+
+echo "Running cargo check"
+cargo check
+
+echo "Committing to dev..."
+git add . 
+git commit -m "$MESSAGE"
+
+echo "Merging dev into main and pushing ..."
+git checkout main
+git merge dev
+git push origin main
+
+
+git checkout dev
+
+echo "Deployed."
