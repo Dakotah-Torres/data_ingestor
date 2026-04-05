c19b0bae322411783302c5e0316c6115b83102f4
Dakotah-Torres <dtorres@dtdevtech.com>
2026-04-05 16:01:37 -0500

    adding dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use tokio_tungstenite::connect_async;
+use url::Url;
+use futures_util::StreamExt;
+
+#[tokio::main]
+async fn main() {
+    let socket_url = "wss://stream.binance.com:9443/ws/btcusdt@trade";
+    let url = Url::parse(socket_url).expect("Invalid URL");
+
+    // This is the 'Handshake'
+    let (ws_stream, _) = connect_async(url).await.expect("Failed to connect");
+    println!("Connected to Binance Firehose!");
+
+    let (_write, mut read) = ws_stream.split();
+
+    // The 'while let' loop stays open as long as Binance keeps sending data
+    while let Some(message) = read.next().await {
+        match message {
+            Ok(_) => println!("New Market Event Received!"),
+            Err(e) => eprintln!("Error receiving message: {}", e),
+        }
+    }
 }
